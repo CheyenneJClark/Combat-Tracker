586b82145836390c4b5322638196fcdb5b4c7e51
Cheyenne Clark <cheyenne@noble-tree.com>
2014-06-20 19:12:50 -0500

    Can now update names and toggle alive/dead.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,21 @@
-var app = {};
+'use strict';
+
+var app = {}; //app namespace
+
+
+//Models
 
 app.Party = Backbone.Model.extend({
 	defaults: {
 		name: '',
 		dead: false
-	}
+	},
+    toggle: function(){
+        this.save({ dead: !this.get('dead') });
+    }
 });
+
+//Collections
 
 app.Combatants = Backbone.Collection.extend({
     model: app.Party,
@@ -13,15 +23,47 @@ app.Combatants = Backbone.Collection.extend({
 });
 app.combatants = new app.Combatants();
 
+//Views
+//Renders each combatant
 app.CombatantView = Backbone.View.extend({
     tagName: 'li',
     template: _.template($('#combatant-template').html()),
     render: function(){
     	this.$el.html(this.template(this.model.toJSON()));
+        this.input = this.$('.edit');
     	return this;
+    },
+    initialize: function(){
+        this.model.on('change', this.render, this);
+    },
+    events:{
+        'dblclick label': 'edit',
+        'keypress .edit': 'updateOnEnter',
+        'blur .edit': 'close',
+        'click .toggle': 'toggleDead'
+    },
+    edit: function(){
+        this.$el.addClass('editing');
+        this.input.focus();
+    },
+    close: function(){
+        var value = this.input.val().trim();
+        if(value){
+            this.model.save({name: value});
+        }
+        this.$el.removeClass('editing');
+    },
+    updateOnEnter: function(e){
+        if(e.which == 13){
+            this.close();
+        }
+    },
+    toggleDead: function(){
+        this.model.toggle();
     }
 });
 
+//Renders the full list by calling each combatant
 app.AppView = Backbone.View.extend({
 	el: '#combatants',
 	initialize: function(){
@@ -54,4 +96,7 @@ app.AppView = Backbone.View.extend({
 	} 
 });
 
+
+//Initializers
 app.appView = new app.AppView();
+
